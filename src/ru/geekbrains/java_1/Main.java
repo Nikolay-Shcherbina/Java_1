5adcc8dbc13ca0f1e0834c8dcf138b5695365327
Nikolay Shcherbina <nikolay-v-s@mail.ru>
2020-03-14 14:59:04 +0300

    // 1.	Создать класс "Сотрудник" с полями: Фамилия, зарплата, возраст, должность; // 2.	Конструктор класса должен заполнять эти поля при создании объекта;

---
+++ src/ru/geekbrains/java_1/Main.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.java_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Staffer staffer_1 = new Staffer("Boris", 900, 35, "engineer");
+        Staffer staffer_2 = new Staffer("Nikolay", 1500, 40, "director");
+        Staffer staffer_3 = new Staffer("Petr", 500, 30, "security");
+        Staffer staffer_4 = new Staffer("Natalya", 600, 45, "secretary");
+        Staffer staffer_5 = new Staffer("Vladimir", 800, 25, "engineer");
+
+
+    }
+
+}
