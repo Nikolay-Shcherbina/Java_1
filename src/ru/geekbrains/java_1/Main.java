39cf9a6410d55168cf23b527bf762fcac41fe915
Nikolay Shcherbina <nikolay-v-s@mail.ru>
2020-03-14 18:43:15 +0300

    + 3, 4, 5

--- src/ru/geekbrains/java_1/Main.java
+++ src/ru/geekbrains/java_1/Main.java
@@ -4,11 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Staffer staffer_1 = new Staffer("Boris", 900, 35, "engineer");
-        Staffer staffer_2 = new Staffer("Nikolay", 1500, 40, "director");
-        Staffer staffer_3 = new Staffer("Petr", 500, 30, "security");
-        Staffer staffer_4 = new Staffer("Natalya", 600, 45, "secretary");
-        Staffer staffer_5 = new Staffer("Vladimir", 800, 25, "engineer");
+// Создание объекта с использованием консруктора Staffer.
+        Staffer employee = new Staffer("Ivanov", 900, 35, "engineer");
+
+// 4.	Вывести при помощи методов из пункта 3 ФИО и должность;
+        System.out.println(employee.getSurname() + ": " + employee.getPosition());
+
+// 5.	Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+        Staffer[] arrStaffer = new Staffer [5];
+        arrStaffer[0] = new Staffer("Volodin", 1100, 45, "engineer");
+        arrStaffer[1] = new Staffer("Belov", 1800, 55, "director");
+        arrStaffer[2] = new Staffer("Voronov", 650, 60, "security ");
+        arrStaffer[3] = new Staffer("Zubova", 700, 37, "secretary");
+        arrStaffer[4] = new Staffer("Ubuntuva", 900, 30, "administrator");
+
+        for (int i = 0; i < arrStaffer.length; i++) {
+            if (arrStaffer[i].getAge() > 40) {
+                arrStaffer[i].getFullPrintInfo();
+            }
+        }
 
 
     }
--- src/ru/geekbrains/java_1/Staffer.java
+++ src/ru/geekbrains/java_1/Staffer.java
@@ -1,21 +1,48 @@
 package ru.geekbrains.java_1;
-// 1.	Создать класс "Сотрудник" с полями: Фамилия, зарплата, возраст, должность;
-// 2.	Конструктор класса должен заполнять эти поля при создании объекта;
 
-// 3.	Внутри класса «Сотрудник» написать методы, которые возвращают значение каждого поля;
-// 4.	Вывести при помощи методов из пункта 3 ФИО и должность.
-// 5.	Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+
+
+// 6.	* Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000;
+// 7.	* Подсчитать средние арифметические зарплаты и возраста сотрудников из п.5;
+// 8.	*** Продумать конструктор таким образом, чтобы при создании каждому сотруднику
+//          присваивался личный уникальный идентификационный порядковый номер.
+
+
+import java.sql.SQLOutput;
 
 public class Staffer {
+
+// 1.	Создать класс "Сотрудник" с полями: Фамилия, зарплата, возраст, должность;
     String surname;
     int salary;
     int age;
     String position;
 
+// 2.	Конструктор класса должен заполнять эти поля при создании объекта;
     Staffer(String surname, int salary, int age, String position) {
-
+        this.surname = surname;
+        this.salary = salary;
+        this.age = age;
+        this.position = position;
     }
 
+// 3.	Внутри класса «Сотрудник» написать методы, которые возвращают значение каждого поля;
+    public String getSurname() {
+        return this.surname;
+    }
+    public String getPosition() {
+        return this.position;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+    public int getSalary() {
+        return this.salary;
+    }
+    void getFullPrintInfo() {
+        System.out.println("surname: " + surname + " salary: "  + salary + " age: " + age + " position: " + position);
+    }
 
 
 
