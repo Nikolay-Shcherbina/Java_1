4634c400ad542f3dd53ad69a48a65ed044d0abf8
Nikolay Shcherbina <nikolay-v-s@mail.ru>
2020-03-15 20:40:32 +0300

    + 6

--- src/ru/geekbrains/java_1/Main.java
+++ src/ru/geekbrains/java_1/Main.java
@@ -4,19 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-// Создание объекта с использованием консруктора Staffer.
+// Создание объекта с использованием конструктора Staffer.
         Staffer employee = new Staffer("Ivanov", 900, 35, "engineer");
 
 // 4.	Вывести при помощи методов из пункта 3 ФИО и должность;
         System.out.println(employee.getSurname() + ": " + employee.getPosition());
 
 // 5.	Создать массив из 5 сотрудников. С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
-        Staffer[] arrStaffer = new Staffer [5];
-        arrStaffer[0] = new Staffer("Volodin", 1100, 45, "engineer");
-        arrStaffer[1] = new Staffer("Belov", 1800, 55, "director");
-        arrStaffer[2] = new Staffer("Voronov", 650, 60, "security ");
-        arrStaffer[3] = new Staffer("Zubova", 700, 37, "secretary");
-        arrStaffer[4] = new Staffer("Ubuntuva", 900, 30, "administrator");
+        Staffer[] arrStaffer = new Staffer[5];
+        arrStaffer[0] = new Staffer("Volodin", 125000, 45, "engineer");
+        arrStaffer[1] = new Staffer("Belov", 185000, 55, "director");
+        arrStaffer[2] = new Staffer("Voronin", 55300, 60, "security ");
+        arrStaffer[3] = new Staffer("Zubova", 73000, 37, "secretary");
+        arrStaffer[4] = new Staffer("Ubuntova", 95000, 30, "administrator");
 
         for (int i = 0; i < arrStaffer.length; i++) {
             if (arrStaffer[i].getAge() > 40) {
@@ -24,7 +24,19 @@ public class Main {
             }
         }
 
+        for (int i = 0; i < arrStaffer.length; i++) {
+            if (arrStaffer[i].getAge() > 45) {
+                int a = arrStaffer[i].getSalary();
+                Staffer.addsSalary(a);
+
+            }
+        }
+
+
+
+
 
     }
-
 }
+
+
--- src/ru/geekbrains/java_1/Staffer.java
+++ src/ru/geekbrains/java_1/Staffer.java
@@ -1,24 +1,19 @@
 package ru.geekbrains.java_1;
 
 
-
-// 6.	* Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000;
 // 7.	* Подсчитать средние арифметические зарплаты и возраста сотрудников из п.5;
 // 8.	*** Продумать конструктор таким образом, чтобы при создании каждому сотруднику
 //          присваивался личный уникальный идентификационный порядковый номер.
 
-
-import java.sql.SQLOutput;
-
 public class Staffer {
 
-// 1.	Создать класс "Сотрудник" с полями: Фамилия, зарплата, возраст, должность;
+    // 1.	Создать класс "Сотрудник" с полями: Фамилия, зарплата, возраст, должность;
     String surname;
     int salary;
     int age;
     String position;
 
-// 2.	Конструктор класса должен заполнять эти поля при создании объекта;
+    // 2.	Конструктор класса должен заполнять эти поля при создании объекта;
     Staffer(String surname, int salary, int age, String position) {
         this.surname = surname;
         this.salary = salary;
@@ -26,10 +21,11 @@ public class Staffer {
         this.position = position;
     }
 
-// 3.	Внутри класса «Сотрудник» написать методы, которые возвращают значение каждого поля;
+    // 3.	Внутри класса «Сотрудник» написать методы, которые возвращают значение каждого поля;
     public String getSurname() {
         return this.surname;
     }
+
     public String getPosition() {
         return this.position;
     }
@@ -37,13 +33,26 @@ public class Staffer {
     public int getAge() {
         return this.age;
     }
+
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+
     public int getSalary() {
         return this.salary;
     }
+
     void getFullPrintInfo() {
-        System.out.println("surname: " + surname + " salary: "  + salary + " age: " + age + " position: " + position);
+        System.out.println("surname: " + surname + " salary: " + salary + " age: " + age + " position: " + position);
     }
 
+// 6.	* Создать метод, повышающий зарплату всем сотрудникам старше 45 лет на 5000;
+    public static int addsSalary(int a) {
+        a = a + 5000;
+        return a;
+    } // не уверен что правильно, т.к. наверное в методе должно просчитываться кому > 45.
 
 
 }
+
+
