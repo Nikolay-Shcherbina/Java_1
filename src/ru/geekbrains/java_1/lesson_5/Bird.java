a4734d64773bb9eeb78c82158f9f7f58ce268f8f
Nikolay-Shcherbina <61752100+Nikolay-Shcherbina@users.noreply.github.com>
2020-03-22 22:13:44 +0300

    Delete Bird.java

--- src/ru/geekbrains/java_1/lesson_5/Bird.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.java_1.lesson_5;
-
-public class Bird extends Animal {
-    public Bird(String name, int running_length, double jumping_height){
-        super(name, running_length, jumping_height);
-    }
-
-}
