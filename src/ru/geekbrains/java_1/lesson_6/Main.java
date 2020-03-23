2f31225bf07044b7e104f51fec865d9e5a9182b2
Nikolay Shcherbina <nikolay-v-s@mail.ru>
2020-03-23 10:00:48 +0300

    Lesson_6

--- src/ru/geekbrains/java_1/lesson_6/Main.java
+++ src/ru/geekbrains/java_1/lesson_6/Main.java
@@ -1,36 +1,51 @@
 package ru.geekbrains.java_1.lesson_6;
-/*
-* 1.	Создать 2 текстовых файла, примерно по 50-100 символов в каждом(особого значения не имеет);
-* 2.	Написать программу, «склеивающую» эти файлы, то есть вначале идет текст из первого файла, потом текст из второго.
-* 3.	* Написать программу, которая проверяет присутствует ли указанное пользователем слово в файле (работаем только с латиницей).
-* 4.	** Написать метод, проверяющий, есть ли указанное слово в папке
-*/
+
 
 import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         try {
             PrintStream psFile1 = new PrintStream(new FileOutputStream("file1.txt"));
-            psFile1.println("HelloWorld");
+            psFile1.print("Hellooooo0123456789AaBbCcDdEeFfGgHhIiGgKkLlMmNnOpPpQqRrSsTtUuVvWwXxYyZz");
             psFile1.close();
 
             PrintStream psFile2 = new PrintStream(new FileOutputStream("file2.txt"));
-            psFile2.println("Hello");
+            psFile2.println("HelloABCDEFGHIGKLMNOPQRSTUVWXYZ");
             psFile2.close();
 
-//            Scanner sc = new Scanner(new FileInputStream("file1.txt"));
-//            System.out.println(sc.next());
-//            sc.close();
+
+
+            Scanner sc = new Scanner(new FileInputStream("file2.txt"));
+            System.out.println(sc.next());
+            sc.close();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+//            try {
+//                FileInputStream fis = new FileInputStream("file1.txt");
+//                int b;
+//                while ((b = fis.read()) != -1)
+//                    System.out.print((char) b);
+//
+//                fis.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+
+
+
+
+
+
     }
 
 }
-
 
